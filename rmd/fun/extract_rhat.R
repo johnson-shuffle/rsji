b78b655be738c0fed48af28d9f3692ccc9d6bbae
johnson-shuffle <jake.j.is@gmail.com>
2018-08-20 21:27:06 -0700

    remove outdated /rmd and mysterious /rsji folders

--- rmd/fun/extract_rhat.R
+++
@@ -1,7 +0,0 @@
-extract_rhat <- function(m) {
-  tibble(
-    model = m,
-    param = row.names(summary(fit[[m]]$result)$summary),
-    rhat  = summary(fit[[m]]$result)$summary[, 'Rhat']
-  )
-}
--- rmd/fun/rpois_rnorm.R
+++
@@ -1,3 +0,0 @@
-rpois_rnorm <- function(N, x, y) {
-  replicate(N, rpois(1, exp(x + rnorm(1, 0, y))))
-}
